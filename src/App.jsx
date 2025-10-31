dc4dd3acc0139b5123a68bb0789cdb1d45f5c882
000ashishYadav <000ashishyadav2003@gmail.com>
2025-10-31 22:28:41 +0530

    feat: implement routing for login, signup, and product pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,38 @@
+import { Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import AddProduct from "./pages/AddProduct";
+import AuthLaout from "./layout/AuthLaout";
+import Login from "./pages/Login";
+import SignUp from "./pages/Signup";
+import NotFoundPage from "./pages/NotFoundPage";
 function App() {
-  return <>Home page</>;
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/login"
+          element={
+            <AuthLaout>
+              <Login />
+            </AuthLaout>
+          }
+        />
+
+        <Route
+          path="/signup"
+          element={
+            <AuthLaout>
+              <SignUp />
+            </AuthLaout>
+          }
+        />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/addProduct" element={<AddProduct />} />
+
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
